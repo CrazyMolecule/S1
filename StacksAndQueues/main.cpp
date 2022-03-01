5b09fcfdfbc7159a9357ae6e996ecec357c06ecd
CrazyMolecule <bavykin.03@mail.ru>
2022-03-02 01:47:42 +0300

    Add tests

--- StacksAndQueues/main.cpp
+++ StacksAndQueues/main.cpp
@@ -2,8 +2,21 @@
 #include "Queue.h"
 #include "Queue.hpp"
 
+#include <vector>;
+
+
 int main()
 {
-    queue<int> myQueue(4);
-    std::cout << myQueue.getCount() << std::endl;
+    queue<int> myQueue; // null constr test
+
+    queue<int> myQueue2(5); // default constr test
+
+    queue<int> myQueue3(myQueue2); // copy constr test
+
+    std::cout << myQueue3.getCount() << std::endl; // getCount test
+
+    std::vector<queue<char*>> v; // !!! #include <vector>;
+    v.push_back(queue<char*>(25)); // move constr test
+
+    myQueue = myQueue2; // copy assignment operator test
 }
