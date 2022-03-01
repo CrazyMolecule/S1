00f215db36a015411e52fab64283863559e19c8f
AeZG <pointongaming12@gmail.com>
2022-03-01 19:56:21 +0300

    Did all the basic stuff, added MAX size for stack.

--- StacksAndQueues/Stack.h
+++ StacksAndQueues/Stack.h
@@ -1,9 +1,26 @@
 #pragma once
 template<typename T>
+#define MAX 100 //max size for stack
 class Stack
 {
 public:
-	void push(T rhs);
-	T drop();
+
+	Stack(); // Null constructor
+	Stack(T* myStack); // Standard constructor
+	Stack(Stack& myStack); // copy constructor
+	Stack(Stack&& value); // move constructor
+	~Stack(); //Destructor
+	
+	Stack& operator=(T* myStack); // default assignment operator
+	Stack& operator=(Stack& myStack); // copy assignment operator
+	Stack& operator=(Stack&& value); // move assignment operator
+
+	void push(T item); // add item to stack
+	T pop(); //remove last item added from stack
+	int getCount(); //get count of items in stack
+	
+private:
+	T* myStack[MAX]; // {bottom <<<<< 1, 2, 3, 4, 5, 6... >>>>>> top}
+	int count; //count of items in stack
 };
 
