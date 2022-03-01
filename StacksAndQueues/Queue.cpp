7cc515c3a5a6aec59fd8875a3a63ad8d59dee6cc
CrazyMolecule <bavykin.03@mail.ru>
2022-03-01 18:35:29 +0300

    Add assignment operator

--- StacksAndQueues/Queue.cpp
+++ StacksAndQueues/Queue.cpp
@@ -1,41 +1,68 @@
 #include "Queue.h"
 
+// null constructor
 template<typename T>
 Queue<T>::Queue()
 {
 }
 
+// default constructor
 template<typename T>
 Queue<T>::Queue(T* queue)
 {
 }
 
+// copy constructor
 template<typename T>
 Queue<T>::Queue(Queue& queue)
 {
 }
 
+// move constructor
 template<typename T>
 Queue<T>::Queue(Queue&& value)
 {
 }
 
+// destructor
+template<typename T>
+Queue<T>::~Queue()
+{
+}
+
+// default assignment operator
+template<typename T>
+Queue<T>& Queue<T>::operator=(T* queue)
+{
+}
+
+// copy assignment operator
+template<typename T>
+Queue<T>& Queue<T>::operator=(Queue& queue)
+{
+}
+
+// move assignment operator
+template<typename T>
+Queue<T>& Queue<T>::operator=(Queue&& value)
+{
+}
+
+// add item to the queue
 template<typename T>
 void Queue<T>::push(T item)
 {
 }
 
+// pop item from queue
 template<typename T>
 T Queue<T>::pop()
 {
 }
 
+// get count of items in queue
 template<typename T>
 int Queue<T>::getCount()
 {
 }
 
-template<typename T>
-Queue<T>::~Queue()
-{
-}
--- StacksAndQueues/Queue.h
+++ StacksAndQueues/Queue.h
@@ -7,11 +7,16 @@ public:
 	Queue(T* queue); // default constructor
 	Queue(Queue& queue); // copy constructor
 	Queue(Queue&& value); // move constructor
-	void push(T item);
-	T pop();
-	int getCount();
 	~Queue(); // destructor
-
+	
+	Queue& operator=(T* queue); // default assignment operator
+	Queue& operator=(Queue& queue); // copy assignment operator
+	Queue& operator=(Queue&& value); // move assignment operator
+	
+	void push(T item); // add item to the queue
+	T pop(); // pop item from queue
+	int getCount(); // get count of items in queue
+	
 private:
 	T* queue; // {T, T, T, T, T, ...}
 	int count; // count of items in {T, T, T, T, T, ...}
