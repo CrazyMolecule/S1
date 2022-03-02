61fc2864c90bbe07f20c398aa5c161022b8e47a8
CrazyMolecule <bavykin.03@mail.ru>
2022-03-03 01:03:17 +0300

    Remove tests

--- StacksAndQueues/main.cpp
+++ StacksAndQueues/main.cpp
@@ -7,30 +7,10 @@
 
 int main()
 {
-    queue<int> myQueue; // null constr test
-
+    queue<int> myQueue(1); // null constr test
+    myQueue.push(5);
     queue<int> myQueue2(5); // default constr test
 
-    myQueue.push(5); // push2 test
+    //myQueue = myQueue + myQueue2;
     std::cout << myQueue << std::endl;
-
-    queue<int> myQueue3(myQueue2); // copy constr test
-
-    std::cout << myQueue3.getCount() << std::endl; // getCount test
-
-    std::vector<queue<char*>> v; // !!! #include <vector>;
-    v.push_back(queue<char*>(25)); // move constr test
-
-    myQueue = myQueue2; // copy assignment operator test
-
-    std::cout << myQueue << std::endl; // operator<< test
-
-    myQueue.push(5); // push2 test
-    std::cout << myQueue << std::endl;
-
-    for (size_t i = 0; i < 5; i++)
-    {
-        myQueue.pop(); // pop test
-        std::cout << myQueue << std::endl;
-    }
 }
