24afdd820f8f123e81271b9ab29511b55f68ca63
CrazyMolecule <bavykin.03@mail.ru>
2022-03-02 22:38:00 +0300

    Add tests

--- StacksAndQueues/main.cpp
+++ StacksAndQueues/main.cpp
@@ -11,6 +11,9 @@ int main()
 
     queue<int> myQueue2(5); // default constr test
 
+    myQueue.push(5); // push2 test
+    std::cout << myQueue << std::endl;
+
     queue<int> myQueue3(myQueue2); // copy constr test
 
     std::cout << myQueue3.getCount() << std::endl; // getCount test
@@ -20,5 +23,14 @@ int main()
 
     myQueue = myQueue2; // copy assignment operator test
 
+    std::cout << myQueue << std::endl; // operator<< test
+
+    myQueue.push(5); // push2 test
     std::cout << myQueue << std::endl;
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        myQueue.pop(); // pop test
+        std::cout << myQueue << std::endl;
+    }
 }
