89ce21a9f9baacf4fc5e49e7c8e65f2beadf943f
AeZG <pointongaming12@gmail.com>
2022-03-02 12:48:40 +0300

    add tests

--- StacksAndQueues/main.cpp
+++ StacksAndQueues/main.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
+#include "Stack.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Stack<int> myStack;
+	return 0; // returns 0
+
+    stack<int> myStack; // null constr test
+
+    stack<int> myStack2(5); // default constr test
+
+    stack<int> myStack3(myStack2); // copy constr test
+
+    std::cout << myStack3.getCount() << std::endl; // getCount test
+
+    std::vector<stack<char*>> v; // !!! #include <vector>;
+    v.push_back(stack<char*>(25)); // move constr test
+
+    myStack = myStack2; // copy assignment operator test
 }
