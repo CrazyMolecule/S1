3292902e8c7bcb88e3090e8207f1ee9806a01990
CrazyMolecule <bavykin.03@mail.ru>
2022-03-05 06:46:24 +0300

    Delete comments

--- StacksAndQueues/main.cpp
+++ StacksAndQueues/main.cpp
@@ -7,29 +7,7 @@
 
 int main(std::string path)
 {
-    /*setlocale(LC_ALL, "ru");
-
-    std::string filename = "filename.txt";
-
-    std::ifstream file(filename);
-    if (!file.is_open())
-    {
-        std::cerr << "Ошибка: Файл ввода не открыт!";
-        return -1;
-    }
-
-    std::string chinchopa;
-    
-    while (!file.eof())
-    {
-        file >> chinchopa;
-        std::cout << chinchopa;
-    }
-
-    file.close();*/
-
     queue<int> myQueue;
-    // std::stack<int> myStack;
 
     return 0;
 }
